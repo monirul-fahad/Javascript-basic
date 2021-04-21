0afd7543ed61b4ab73b168ba4b173862a872b076
unknown <immonirulfahad@gmail.com>
2021-04-21 22:59:58 +0600

    ES6-part2

--- practice-java/stack-learn/chapter-fifteen/chapter15.js
+++ practice-java/stack-learn/chapter-fifteen/chapter15.js
@@ -16,13 +16,13 @@
 
 // New arrow function 
 
-let add = (a, b) => a + b
-console.log(add(45,50))
+// let add = (a, b) => a + b
+// console.log(add(45,50))
 
-// when one peramiter 
+// // when one peramiter 
 
-let double = n => n*n 
-console.log(double(5))
+// let double = n => n*n 
+// console.log(double(5))
 
 // another arrow function 
 
@@ -84,17 +84,17 @@ console.log(double(5))
 
 // DESTRUCTURING 
 
-let person = {
-    name: 'Monirul Islam',
-    email: 'immonirulfahad@gmail.com',
-    address: {
-        city: 'Dhaka',
-        country: 'Bangladesh'
-    }
-}
+// let person = {
+//     name: 'Monirul Islam',
+//     email: 'immonirulfahad@gmail.com',
+//     address: {
+//         city: 'Dhaka',
+//         country: 'Bangladesh'
+//     }
+// }
 
-let { name, email, address, address: {city, country} } = person
-console.log(name, email, city, country)
+// let { name, email, address, address: {city, country} } = person
+// console.log(name, email, city, country)
 
 // Object to array method 
 
@@ -112,3 +112,105 @@ console.log(name, email, city, country)
 //     ['b', 20]
 // ]
 // console.log(Object.fromEntries(objArr))
+
+
+
+
+const arr = [10, 22, 35]
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+        // ITERATOR 
+
+// Symbol.iterator 
+
+// let iterate = arr[Symbol.iterator]()
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+
+// FOR OF LOOP 
+
+// for (let v of arr) {
+//     console.log(v)
+// }
+
+// for (let v of 'MONIRUL ISLAM') {
+//     console.log(v)
+// }
+
+    // GENARATOR 
+
+// function* generate(){
+//     yield 1
+//     yield 2 
+//     yield 3
+// }     
+// let iterate = generate()
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+// console.log(iterate.next())
+
+// for loop 
+function* generate(array) {
+    for (let i = 0; i < array.length; i++) {
+        yield array[i]
+    }
+}
+
+let it = generate([20, 15, 78])
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+
+    //  set 
+
+// let set = new Set([1, 2, 3])
+// set.add(5)
+// set.add(6)
+// set.add(1)
+// set.add(2)
+// console.log(set)
+// console.log(set.size)
+
+// console.log(set.has(5))    
+
+    // map  ]
+
+// let map = new Map([
+//     ['a', 10],
+//     ['b', 20],
+//     ['c', 30]
+// ])
+
+// map.set('d', 40)    
+// console.log(map)
+// console.log(map.get('c'))
+// console.log(map.delete('a'))
+// console.log(map)
+// console.log(map.size)
+
+// for loop 
+// for (let [k, v] of map) {
+//     console.log(v, k)
+// }
+
+// for each 
+// map.forEach((v, k) => {
+//     console.log(k, v)
+// })
+
+// WEAK MAP 
+
+let weakMap = new WeakMap([[a, 45], [b, 75]])
+
+a = null
+
+console.log(weakMap.get(a))
+console.log(weakMap.has(a))
+console.log(weakMap.has(b))
